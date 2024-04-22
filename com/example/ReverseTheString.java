84e1cd37511861d56a3201732efe18c990007d76
Spencer S <spencer.scarlet@stu.ocsb.ca>
2024-04-22 23:53:55 +0000

    Fix and done

--- com/example/ReverseTheString.java
+++ com/example/ReverseTheString.java
@@ -1,5 +1,12 @@
+//package com.example;
 
-package com.example;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 /**
  * Change me.
@@ -23,6 +30,56 @@ public final class ReverseTheString {
      * @param args Unused
      */
     public static void main(final String[] args) {
-        System.out.println("Hello, world");
+        // declare opening list
+        final ArrayList<String> stringList = new ArrayList<String>();
+
+        try {
+            // preps file access and writing/reading
+            final File fileInput = new File("input.txt");
+            final File fileOutput = new File("output.txt");
+            final Scanner sc = new Scanner(fileInput);
+            final FileWriter fW = new FileWriter(fileOutput);
+            final PrintWriter write = new PrintWriter(fW);
+
+            // reads each line of file
+            while (sc.hasNextLine()) {
+                // adds the next file
+                stringList.add(sc.nextLine());
+            }
+
+            // reverse each string in the list than writes to output
+            for (String inputString : stringList) {
+                String revString = RecReverse(inputString);
+                write.println(revString);
+            }
+
+            // program finished
+            System.out.println("Done");
+
+            // closes resources
+            write.close();
+            sc.close();
+
+        } catch (IOException err) {
+            // For when no input file is found.
+            System.err.println("Error: " + err.getMessage());
+        }
+    }
+
+    /**
+     * Reverses the strings
+     *
+     * @param stringList
+     * @return reversed string list
+     */
+    public static String RecReverse(String stringList) {
+        // if no characters are left end function 
+        if (stringList.length() == 0) {
+            return stringList;
+        }
+        // if there is a string left, reverse the strings
+        else {
+            return RecReverse(stringList.substring(1)) + stringList.charAt(0);
+        }
     }
 }
